2014917b4c3ce19626f5a8f2c60d67fc4154ac80
ATT-OAST <ms6926@att.com>
2022-10-14 08:05:44 -0700

    my attempts so far

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path'
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloResolver } from './hello.resolver';
@@ -13,8 +13,9 @@ import { HelloResolver } from './hello.resolver';
       typePaths: ['./**/*.graphql'],
       definitions: {
         path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
       },
-      playground: true
+      playground: true,
     }),
   ],
   controllers: [AppController],
--- src/graphql.ts
+++ src/graphql.ts
@@ -7,8 +7,36 @@
 
 /* tslint:disable */
 /* eslint-disable */
-export interface IQuery {
-    hello(): string | Promise<string>;
+
+export abstract class IQuery {
+    abstract hello(): string | Promise<string>;
+}
+
+export class Consultant {
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+    country: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export class Engagement {
+    id: string;
+    name: string;
+    description: string;
+    start_date: string;
+    end_date: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+    consultant: Nullable<Consultant>[];
 }
 
 type Nullable<T> = T | null;
